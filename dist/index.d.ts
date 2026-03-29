3bc408eb773ad38d1f08692087208478043431f2
Jason Shen <jason_2000_nz@hotmail.com>
2026-03-29 19:47:22 +1300

    fixed gitignore

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { StreamCoreAIClient } from "./client.js";
-export { whipOffer, whipDelete } from "./whip.js";
-export type { ConnectionStatus, TranscriptEntry, TimingEvent, DataChannelMessage, StreamCoreAIConfig, StreamCoreAIEvents, } from "./types.js";
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export { StreamCoreAIClient } from "./client.js";
-export { whipOffer, whipDelete } from "./whip.js";
-//# sourceMappingURL=index.js.map
